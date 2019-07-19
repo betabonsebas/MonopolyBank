0921fbab8baa6c943362cc01a1c3f1ccbacfd93c
Sebastian Bonilla <betabonsebas@gmail.com>
2019-07-18 20:18:34 -0500

    Initial Commit

---
+++ MonopolyBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonopolyBank
+//
+//  Created by Sebastian Bonilla on 7/18/19.
+//  Copyright © 2019 bonsebas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
